4499e7211e08b200ca11ad79cc35e33e0c8da454
unknown <you@example.com>
2019-12-28 17:19:15 +0200

    3d cards done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Поворот карты по клику
+$('.card-block').click(function () {
+  if ($(this).find('.card').hasClass('flip')) {
+    $(this).find('.card').removeClass('flip')
+
+  } else {
+    $(this).find('.card').addClass('flip')
+  }
+})
+
+// Поворот карты по ховеру
+// $('.card-block').hover( function() {
+// 	$(this).find('.card').addClass('flip')
+// },
+// function() {
+// 	$(this).find('.card').removeClass('flip')
+// })
+
+$(function () {
+
+  $('.draggable').draggable();
+
+});
